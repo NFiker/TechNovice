8d31c33711b40e8a20ee2a6b392c18d2386da1e0
Etudiant O'clock <nicolas.bagat@oclock.school>
2024-09-03 00:37:14 +0200

    prisma initialized

--- api/app.js
+++ api/app.js
@@ -1,9 +1,13 @@
-const express = require('express');
+import express from 'express';
 
 const app = express();
-const PORT = 3000;
 
-app.listen(PORT, error => {
-    if (!error) console.log('Server is Successfully Running, and App is listening on port ' + PORT);
-    else console.log("Error occurred, server can't start", error);
+const port = 3000;
+
+app.listen(port, error => {
+    if (!error) {
+        console.log('Server is Successfully Running, and App is listening on port ' + port);
+    } else {
+        console.log("Error occurred, server can't start", error);
+    }
 });

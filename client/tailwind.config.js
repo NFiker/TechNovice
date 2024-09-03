bdf05d02b88241198931e2cc80558250f8115efc
horameus <ludo.hartmann@gmail.com>
2024-09-03 12:05:02 +0200

    release/0.1/base_homepage_3 : font-family

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,10 @@
 export default {
     content: ['*'],
     theme: {
+        fontFamily: {
+            montserrat: ['Montserrat', 'sans-serif'],
+            DMSerifDisplay: ['"DM Serif Display"', 'serif'],
+        },
         extend: {},
     },
     plugins: [],

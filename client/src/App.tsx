742ad58e8ddf757bbdba1940fffd0fc49b14e35a
horameus <ludo.hartmann@gmail.com>
2024-09-04 13:45:20 +0200

    release/0.1/homepage_react : header react OK

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,8 @@
 import { FaCheck, FaTimes } from 'react-icons/fa';
 import Button from './components/reusable-ui/Button';
-import CourseCard, { CourseType } from './components/reusable-ui/CourseCard';
+import type { CourseType } from './components/reusable-ui/CourseCard';
+import CourseCard from './components/reusable-ui/CourseCard';
+import Header from './components/reusable-ui/Header';
 
 export default function App() {
     // Données factices pour 12 cours
@@ -16,36 +18,39 @@ export default function App() {
     }));
 
     return (
-        <main className="App py-8 px-4 bg-gradient-to-r from-indigo-600 via-indigo-700 to-indigo-800 h-48 w-full rounded-lg shadow-md p-4">
-            <div className="max-w-7xl mx-auto">
-                <h1 className="text-2xl font-bold text-grey-400 mb-6">Catalogue des cours</h1>
-                {/* Configuration de la grille pour s'adapter à différents écrans */}
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-                    {mockCourseData.map(course => (
-                        <CourseCard key={course.id} course={course} />
-                    ))}
+        <>
+            <Header />
+            <main className="App py-8 px-4 bg-gradient-to-r from-indigo-600 via-indigo-700 to-indigo-800 h-48 w-full rounded-lg shadow-md p-4">
+                <div className="max-w-7xl mx-auto">
+                    <h1 className="text-2xl font-bold text-grey-400 mb-6">Catalogue des cours</h1>
+                    {/* Configuration de la grille pour s'adapter à différents écrans */}
+                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+                        {mockCourseData.map(course => (
+                            <CourseCard key={course.id} course={course} />
+                        ))}
+                    </div>
+                    <div className="p-8 space-y-4">
+                        <Button
+                            label="Primary Button"
+                            version="primary"
+                            onClick={() => alert('Primary clicked!')}
+                        />
+                        <Button
+                            label="Success Button"
+                            version="success"
+                            onClick={() => alert('Success clicked!')}
+                            Icon={<FaCheck />}
+                        />
+                        <Button
+                            label="Danger Button"
+                            version="danger"
+                            onClick={() => alert('Danger clicked!')}
+                            Icon={<FaTimes />}
+                        />
+                        <Button label="Disabled Button" version="primary" disabled={true} />
+                    </div>
                 </div>
-                <div className="p-8 space-y-4">
-                    <Button
-                        label="Primary Button"
-                        version="primary"
-                        onClick={() => alert('Primary clicked!')}
-                    />
-                    <Button
-                        label="Success Button"
-                        version="success"
-                        onClick={() => alert('Success clicked!')}
-                        Icon={<FaCheck />}
-                    />
-                    <Button
-                        label="Danger Button"
-                        version="danger"
-                        onClick={() => alert('Danger clicked!')}
-                        Icon={<FaTimes />}
-                    />
-                    <Button label="Disabled Button" version="primary" disabled={true} />
-                </div>
-            </div>
-        </main>
+            </main>
+        </>
     );
 }

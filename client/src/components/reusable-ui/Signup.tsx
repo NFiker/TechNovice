aedcd0ff916464302188d76f6fa17db763eb27ed
horameus <ludo.hartmann@gmail.com>
2024-09-09 17:37:23 +0200

    release/0.1/homepage_finition_31 : signup modal done

--- client/src/components/reusable-ui/Signup.tsx
+++ client/src/components/reusable-ui/Signup.tsx
@@ -8,16 +8,17 @@ const Signup: React.FC = () => {
                 <div className="flex flex-col justify-center items-center h-full select-none p-6 bg-indigo-600 rounded-t-xl">
                     <img className="max-w-20" src="img/logo-small.png" alt="" />
                 </div>
-                <div className="min-h-96 px-8 py-6 text-left bg-white rounded-b-xl shadow-lg">
+                <div className="min-h-96 px-8 py-4 text-left bg-white rounded-b-xl shadow-lg">
                     <div className="flex flex-col justify-center items-center h-full select-none">
-                        <div className="flex flex-col items-center justify-center gap-2 mb-8">
+                        <div className="flex flex-col items-center justify-center gap-2 mb-4">
                             <h1 className="m-0 font-semibold text-2xl">Inscrivez-vous!</h1>
                             <span className="m-0 text-xs text-center">
-                                Connectez vous à votre compte pour accéder à tous nos services
+                                Rejoignez-nous pour accéder à tous nos services, c'est gratuit et ça ne vous
+                                prendra que deux minutes!
                             </span>
                         </div>
                     </div>
-                    <div className="w-full flex flex-col gap-2">
+                    <div className="w-full flex flex-col gap-1">
                         <label className="font-semibold">Email</label>
                         <input
                             className="border rounded-lg px-3 py-2 mb-5 text-sm w-full"
@@ -26,7 +27,25 @@ const Signup: React.FC = () => {
                             name="email"
                         />
                     </div>
-                    <div className="w-full flex flex-col gap-2">
+                    <div className="w-full flex flex-col gap-1">
+                        <label className="font-semibold">Prénom</label>
+                        <input
+                            className="border rounded-lg px-3 py-2 mb-5 text-sm w-full"
+                            placeholder="Votre prénom..."
+                            type="text"
+                            name="firstname"
+                        />
+                    </div>
+                    <div className="w-full flex flex-col gap-1">
+                        <label className="font-semibold">Nom</label>
+                        <input
+                            className="border rounded-lg px-3 py-2 mb-5 text-sm w-full"
+                            placeholder="Votre nom..."
+                            type="text"
+                            name="lastname"
+                        />
+                    </div>
+                    <div className="w-full flex flex-col gap-1">
                         <label className="font-semibold">Mot de passe</label>
                         <input
                             type="password"
@@ -35,18 +54,27 @@ const Signup: React.FC = () => {
                             placeholder="••••••••"
                         />
                     </div>
+                    <div className="w-full flex flex-col gap-1">
+                        <label className="font-semibold">Confirmez votre mot de passe</label>
+                        <input
+                            type="password"
+                            name="password-confirm"
+                            className="border rounded-lg px-3 py-2 mb-5 text-sm w-full"
+                            placeholder="••••••••"
+                        />
+                    </div>
                     <div className="mt-5">
                         <button
                             type="submit"
                             className="py-1 px-8 bg-sky-500 hover:bg-sky-700 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg cursor-pointer select-none">
-                            Se connecter
+                            S'inscrire
                         </button>
                     </div>
                     <div className="w-full flex flex-col gap-2 mt-4">
                         <a
                             className="font-semibold text-sm text-center text-indigo-600 hover:underline hover:underline-offset-2"
                             href="">
-                            Pas de compte ? inscrivez-vous ici !
+                            Déjà un compte ? Connectez-vous ici !
                         </a>
                     </div>
                 </div>

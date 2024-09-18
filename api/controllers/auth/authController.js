59c6094f77b37ae18e972ec647c6954ab032ee15
Etudiant O'clock <nicolas.bagat@oclock.school>
2024-09-18 14:35:26 +0200

    Login fix attempt

--- api/controllers/auth/authController.js
+++ api/controllers/auth/authController.js
@@ -22,18 +22,14 @@ const authController = {
             if (!match) {
                 return res.status(400).json({ message: 'Mot de passe incorrect' });
             }
-            console.log('Tentative de création du token');
+
             const jwToken = jwt.sign({ id: user.user_id }, process.env.ACCESS_TOKEN_SECRET, {
                 expiresIn: '1h',
             });
-            console.log('Token créé');
-            console.log(jwToken.data);
-            return res.json({
-                message: `Bienvenue ${user.first_name}`,
-                jwToken,
-            });
+
+            return res.json({ jwToken });
         } catch (error) {
-            return res.status(500).json({ message: 'Erreur lors de la connexion', error });
+            return res.status(500).json({ message: 'toto', error });
         } finally {
             prisma.$disconnect();
         }
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -31,10 +31,14 @@ function App() {
 
     useEffect(() => {
         if (localStorage.getItem('token')) {
-            api.get('/api/users/:user_id')
+            api.get('/my-infos')
                 .then(response => {
                     setUser({
                         user_id: response.data.user_id,
+                        nickname: response.data.nickname,
+                        mail: response.data.mail,
+                        first_name: response.data.first_name,
+                        last_name: response.data.last_name,
                     });
                 })
                 .catch(error => {
@@ -59,11 +63,9 @@ function App() {
                 {/* Pages de détail */}
                 <Route path="/inscription" element={<Signup />} />
                 <Route path="/sujet/:id" element={<TopicDetail />} /> {/* Route pour TopicDetail */}
-
                 <Route path="/courses/:course_id" element={<CourseDetail />} />{' '}
                 {/* Route pour CourseDetail */}
                 {/* <Route path="/enseignant/:id" element={<TeacherDetail />} /> */}
-
                 {/* Route pour TeacherDetail */}
                 <Route
                     path="/tableau/:user_id"
